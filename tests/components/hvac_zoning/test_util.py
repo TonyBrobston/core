aa40a2c9cb513ab8d63a803db363a7084090067d
Tony Brobston <anthonybrobston@live.com>
2024-05-10 19:10:06 +0000

    Refactor

--- tests/components/hvac_zoning/test_util.py
+++ tests/components/hvac_zoning/test_util.py
@@ -24,23 +24,11 @@ def test_reformat_and_filter_to_valid_areas() -> None:
                 "cover.kitchen_south_vent",
                 "cover.kitchen_northwest_vent",
             ],
-            "basement": [
-                "cover.basement_west_vent",
-                "cover.basement_northeast_vent",
-                "cover.basement_southeast_vent",
-            ],
             "master_bedroom": ["cover.master_bedroom_vent"],
-            "guest_bedroom": ["cover.guest_bedroom_vent"],
-            "office": ["cover.office_vent"],
-            "upstairs_bathroom": ["cover.upstairs_bathroom_vent"],
         },
         "temperature": {
             "main_floor": "sensor.main_floor_temperature",
-            "basement": "sensor.basement_temperature",
             "master_bedroom": "sensor.master_bedroom_temperature",
-            "guest_bedroom": "sensor.guest_bedroom_temperature",
-            "office": "sensor.office_temperature",
-            "upstairs_bathroom": "sensor.upstairs_bathroom_temperature",
         },
         "climate": {"main_floor": "climate.living_room_thermostat"},
     }
@@ -48,18 +36,6 @@ def test_reformat_and_filter_to_valid_areas() -> None:
     areas = reformat_and_filter_to_valid_areas(user_input)
 
     assert areas == {
-        "basement": {
-            "damper": [
-                "cover.basement_west_vent",
-                "cover.basement_northeast_vent",
-                "cover.basement_southeast_vent",
-            ],
-            "temperature": "sensor.basement_temperature",
-        },
-        "guest_bedroom": {
-            "damper": ["cover.guest_bedroom_vent"],
-            "temperature": "sensor.guest_bedroom_temperature",
-        },
         "main_floor": {
             "damper": [
                 "cover.living_room_northeast_vent",
@@ -73,88 +49,25 @@ def test_reformat_and_filter_to_valid_areas() -> None:
             "damper": ["cover.master_bedroom_vent"],
             "temperature": "sensor.master_bedroom_temperature",
         },
-        "office": {
-            "damper": ["cover.office_vent"],
-            "temperature": "sensor.office_temperature",
-        },
-        "upstairs_bathroom": {
-            "damper": ["cover.upstairs_bathroom_vent"],
-            "temperature": "sensor.upstairs_bathroom_temperature",
-        },
     }
-
-
-def test_filter_to_valid_areas() -> None:
-    """Test filter to valid areas."""
-    user_input = {
-        "damper": {
-            "main_floor": [
-                "cover.living_room_northeast_vent",
-                "cover.living_room_southeast_vent",
-                "cover.kitchen_south_vent",
-                "cover.kitchen_northwest_vent",
-            ],
-            "basement": [
-                "cover.basement_west_vent",
-                "cover.basement_northeast_vent",
-                "cover.basement_southeast_vent",
-            ],
-            "master_bedroom": ["cover.master_bedroom_vent"],
-            "guest_bedroom": ["cover.guest_bedroom_vent"],
-            "office": ["cover.office_vent"],
-            "upstairs_bathroom": ["cover.upstairs_bathroom_vent"],
-        },
-        "temperature": {
-            "main_floor": "sensor.main_floor_temperature",
-            "basement": "sensor.basement_temperature",
-            "master_bedroom": "sensor.master_bedroom_temperature",
-            "guest_bedroom": "sensor.guest_bedroom_temperature",
-            "office": "sensor.office_temperature",
-            "upstairs_bathroom": "sensor.upstairs_bathroom_temperature",
-        },
-        "climate": {"main_floor": "climate.living_room_thermostat"},
-    }
-
-    areas = filter_to_valid_areas(user_input)
-
-    assert areas == [
-        "basement",
-        "guest_bedroom",
-        "main_floor",
-        "master_bedroom",
-        "office",
-        "upstairs_bathroom",
-    ]
 
 
 def test_filter_to_valid_areas_missing_temperature() -> None:
     """Test filter to valid areas missing temperature."""
     user_input = {
         "damper": {
-            "main_floor": [
-                "cover.living_room_northeast_vent",
-                "cover.living_room_southeast_vent",
-                "cover.kitchen_south_vent",
-                "cover.kitchen_northwest_vent",
-            ],
             "basement": [
                 "cover.basement_west_vent",
                 "cover.basement_northeast_vent",
                 "cover.basement_southeast_vent",
             ],
-            "master_bedroom": ["cover.master_bedroom_vent"],
             "guest_bedroom": ["cover.guest_bedroom_vent"],
-            "office": ["cover.office_vent"],
             "upstairs_bathroom": ["cover.upstairs_bathroom_vent"],
         },
         "temperature": {
-            "main_floor": "sensor.main_floor_temperature",
             "basement": "sensor.basement_temperature",
-            "master_bedroom": "sensor.master_bedroom_temperature",
             "guest_bedroom": "sensor.guest_bedroom_temperature",
-            "office": "sensor.office_temperature",
         },
-        "climate": {"main_floor": "climate.living_room_thermostat"},
     }
 
     areas = filter_to_valid_areas(user_input)
@@ -162,9 +75,6 @@ def test_filter_to_valid_areas_missing_temperature() -> None:
     assert areas == [
         "basement",
         "guest_bedroom",
-        "main_floor",
-        "master_bedroom",
-        "office",
     ]
 
 
@@ -172,34 +82,20 @@ def test_filter_to_valid_areas_missing_damper() -> None:
     """Test filter to valid areas missing damper."""
     user_input = {
         "damper": {
-            "basement": [
-                "cover.basement_west_vent",
-                "cover.basement_northeast_vent",
-                "cover.basement_southeast_vent",
-            ],
-            "master_bedroom": ["cover.master_bedroom_vent"],
             "guest_bedroom": ["cover.guest_bedroom_vent"],
-            "office": ["cover.office_vent"],
             "upstairs_bathroom": ["cover.upstairs_bathroom_vent"],
         },
         "temperature": {
             "main_floor": "sensor.main_floor_temperature",
-            "basement": "sensor.basement_temperature",
-            "master_bedroom": "sensor.master_bedroom_temperature",
             "guest_bedroom": "sensor.guest_bedroom_temperature",
-            "office": "sensor.office_temperature",
             "upstairs_bathroom": "sensor.upstairs_bathroom_temperature",
         },
-        "climate": {"main_floor": "climate.living_room_thermostat"},
     }
 
     areas = filter_to_valid_areas(user_input)
 
     assert areas == [
-        "basement",
         "guest_bedroom",
-        "master_bedroom",
-        "office",
         "upstairs_bathroom",
     ]
 
